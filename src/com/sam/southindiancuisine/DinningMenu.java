094d5dbe1e32d9475407a554029c9bab6cfb855d
CArunSamuel <Arunkrish31@gmail.com>
2026-09-01 11:59:02 +0530

    Dinning-Menu-class-completed

---
+++ src/com/sam/southindiancuisine/DinningMenu.java
@@ -0,0 +1,16 @@
+package com.sam.southindiancuisine;
+
+public class DinningMenu {
+
+    String foodCategory;
+    double price;
+    String foodName;
+
+    DinningMenu(String foodCategory, double foodPrice, String foodName) {
+
+        this.foodCategory = foodCategory;
+        this.price = foodPrice;
+        this.foodName = foodName;
+    }
+}
+
