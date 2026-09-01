a889e71704c8c55c2474f137398bb18f17bccfaa
CArunSamuel <Arunkrish31@gmail.com>
2026-09-01 15:44:55 +0530

    //Intiated-Original-Dining-Menu

---
+++ src/com/sam/southindiancuisine/RestaurantBillApp.java
@@ -0,0 +1,23 @@
+package com.sam.southindiancuisine;
+
+public class RestaurantBillApp {
+
+    DinningMenu food1 = new DinningMenu("Breakfast", "Dosa", 60.50);
+    DinningMenu food2 = new DinningMenu("Breakfast", "Idli", 40);
+
+    DinningMenu food3 = new DinningMenu("Soup", "Veg Pepper Soup", 50);
+    DinningMenu food4 = new DinningMenu("Soup", "Tomato Soup", 45);
+
+    DinningMenu food5 = new DinningMenu("Uthappam", "Onion Uthappam", 150);
+    DinningMenu food6 = new DinningMenu("Uthappam", "Paneer Uthappam", 180);
+
+    RestaurantBill bill = new RestaurantBill();
+
+
+
+    System.out.println("================================");
+    System.out.println("    WELCOME TO ANANDHA BHAVAN  ");
+    System.out.println("================================");
+
+
+}
